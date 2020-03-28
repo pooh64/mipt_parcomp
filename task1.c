0316bb86a29687c2e77e22173913cea98db309b7
Vsevolod Pukhov <pukhov.vyu@phystech.edu>
2020-03-28 15:17:43 +0300

    task1 ttysync

--- task1.c
+++ task1.c
@@ -1,5 +1,26 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define print_trace() 						\
+do {								\
+	printf("[%.3d]: print_trace: %s: (" __FILE__ ": %d)\n",	\
+			get_mpi_rank(), __func__, __LINE__);	\
+	fflush(stdout);						\
+} while (0)
+
+#define print_dbg(fmt, ...) 					\
+do {								\
+	printf("[%.3d]: ", get_mpi_rank());			\
+	printf(fmt, ##__VA_ARGS__);				\
+	fflush(stdout);						\
+} while (0)
 
 int get_mpi_rank()
 {
@@ -25,12 +46,41 @@ void recv_int(int src, int tag, int *msg, MPI_Status *status)
 	MPI_Recv(msg, 1, MPI_INT, src, tag, MPI_COMM_WORLD, status);
 }
 
-#define print_trace() 						\
-do {								\
-	printf("[%.3d]: print_trace: %s: (" __FILE__ ": %d)\n",	\
-			get_mpi_rank(), __func__, __LINE__);	\
-	fflush(stdout);						\
-} while (0)
+FILE *tty_file;
+
+void ttysync_root()
+{
+	char buf[32] = { };
+	ttyname_r(fileno(stdout), buf, sizeof(buf));
+
+	int size = get_mpi_size();
+	for (int i = 1; i < size; ++i)
+		MPI_Send(buf, sizeof(buf), MPI_CHAR, i, 0, MPI_COMM_WORLD);
+
+	tty_file = fopen(buf, "w");
+	assert(tty_file);
+}
+
+void ttysync_node()
+{
+	char buf[32] = { };
+	MPI_Recv(buf, sizeof(buf), MPI_CHAR, 0, 0, MPI_COMM_WORLD, NULL);
+	tty_file = fopen(buf, "w");
+	assert(tty_file);
+}
+
+void ttysync()
+{
+	if (get_mpi_rank() == 0)
+		ttysync_root();
+	else
+		ttysync_node();
+}
+
+void print_hello()
+{
+	fprintf(tty_file, "[%.3d]\n", get_mpi_rank());
+}
 
 void test1()
 {
@@ -41,7 +91,7 @@ void test1()
 	if (rank != 0)
 		recv_int(rank - 1, 0, &dummy_msg, NULL);
 
-	print_trace();
+	print_hello();
 
 	if (rank != size - 1)
 		send_int(rank + 1, 0, &dummy_msg);
@@ -55,11 +105,11 @@ void test2()
 
 	if (rank != 0) {
 		recv_int(0, 0, &dummy_msg, NULL);
-		print_trace();
+		print_hello();
 		send_int(0, 0, &dummy_msg);
 	} else {
-		print_trace();
-		for (int i = 0; i < size; ++i) {
+		print_hello();
+		for (int i = 1; i < size; ++i) {
 			send_int(i, 0, &dummy_msg);
 			recv_int(i, 0, &dummy_msg, NULL);
 		}
@@ -69,8 +119,13 @@ void test2()
 int main(int argc, char **argv)
 {
 	MPI_Init(&argc, &argv);
+	ttysync();
+
+#ifdef 0
 	test1();
-	//test2();
+#else
+	test2()
+#endif
 	MPI_Finalize();
 	return 0;
 }
